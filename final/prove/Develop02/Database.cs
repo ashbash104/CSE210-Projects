6a560cb18b64bd3b7b3e0f6b6e71c5dbebdf7e3d
ashbash104 <ashlee.hart104@gmail.com>
2023-01-28 22:52:59 -0700

    database update

---
+++ final/prove/Develop02/Database.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Develop02
+{
+    public class Database
+{
+  private List<JournalEntries> entries;
+
+    public Database()
+    {
+        entries = new List<JournalEntries>();
+    }
+    public List<JournalEntries> SearchEntries(DateTime date,
+                                            bool byTime)
+{
+    List<JournalEntries> found = new List<JournalEntries>();
+    foreach (JournalEntries entry in JournalEntries)
+    {
+        if ((!byTime) && (entry.Occurs.Date == date.Date))
+            found.Add(entry);
+    }
+    return found;
+}
+}
+
+    internal class JournalEntries
+    {
+        public object Occurs { get; internal set; }
+    }
+}
