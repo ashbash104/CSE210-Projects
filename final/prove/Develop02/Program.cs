b14db999f937aa3dc005ad0699580353738353a6
ashbash104 <ashlee.hart104@gmail.com>
2023-01-28 22:46:34 -0700

    Develop02 Progress

--- final/prove/Develop02/Program.cs
+++ final/prove/Develop02/Program.cs
@@ -1,9 +1,45 @@
 using System;
 
-class Program
+namespace Develop02
 {
+    class Program
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        JournalEntries journal = new JournalEntries();
+        char option = '0';
+        while(option != '4')
+        {
+            journal.PrintHomeScreen();
+            Console.WriteLine("Choose an action: ");
+            Console.WriteLine("1 - Add a journal entry");
+            Console.WriteLine("2 - Search for journal entries");
+            Console.WriteLine("3 - Delete journal entries");
+            Console.WriteLine("4 - Finish");
+            //still need to set variable option above
+            option = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            switch (option)
+            {
+                case '1':
+                    journal.PrintPrompts();
+                    journal.AddEntry();
+                    break;
+                case '2':
+                    journal.SearchEntries();
+                    break;
+                case '3':
+                    journal.DeleteEntries();
+                    break;
+                    case '4':
+                    Console.WriteLine("Press any key to quit the program");
+                    break;
+                default:
+                    Console.WriteLine("Error. Press any key to choose from options.");
+                    break;
+            }
+            Console.ReadKey();
+        }
+        
     }
+
 }
