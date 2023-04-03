9ba4f3d87c3f45f4a4745d5a5982074f6d4fe732
ashbash104 <ashlee.hart104@gmail.com>
2023-04-03 16:34:17 -0600

    Foundation 4 Done

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        //Console.WriteLine("Hello Foundation4 World!");
+        Console.WriteLine("Exercise Tracking");
+        Console.WriteLine("Summary:");
+
+        List<Activity> _activities = new List<Activity>();
+
+        Running _ra = new Running("April 3rd, 2023", "Running", 20.00, 2.5);
+        _activities.Add(_ra);
+
+        Cycling _ca = new Cycling("April 1st, 2023", "Cycling", 120.00, 40.00);
+        _activities.Add(_ca);
+
+        Swimming _sa = new Swimming("March 31st, 2023", "Swimming", 30.00, 15.00);
+        _activities.Add(_sa);
+
+        Console.WriteLine();
+
+        foreach (Activity a in _activities)
+        {
+            string _na = a.GetNameActivity();
+            Console.WriteLine(_na.ToUpper());
+            double distance = a.GetDistance();
+            double speed = a.GetSpeed();
+            double pace = a.GetPace();
+            Console.WriteLine($"{a.GetDate()} - {a.GetNameActivity()} ({a.GetTime()} min) - Distance: {a.GetDistance()} km - Speed: {a.GetSpeed()} kph - Pace: {a.GetPace()} min per km");
+            Console.WriteLine();
+        }
     }
 }
--- final/Foundation4/Swimming.cs
+++ final/Foundation4/Swimming.cs
@@ -1,6 +1,22 @@
 using System;
 
-class Swimming
+class Swimming : Activity
 {
-
+    private double _laps;
+    public Swimming(string date, string nameActivity, double time, double laps) : base(date, nameActivity, time)
+    {
+        _laps = laps;
+    }
+    public override double GetDistance()
+    {
+        return _laps * 50 / 1000;
+    }
+    public override double GetSpeed()
+    {
+        return (GetDistance() / _time) * 60;
+    }
+    public override double GetPace()
+    {
+        return 60 / GetSpeed();
+    }
 }
